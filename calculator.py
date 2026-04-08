9bf589ea3ba7f97bf3b029dba7201eb647015206
zgunning-sudo <zgunning@ufl.edu>
2026-04-08 11:15:48 -0400

    Fully correct version

--- calculator.py
+++ calculator.py
@@ -2,15 +2,15 @@ import math
 
 def square_root(a):
     try:
-        math.sqrt(a)
+        return math.sqrt(a)
     except ValueError:
-        print("Invalid Number")
+        raise
 
 def hypotenuse(a, b):
     try:
-        math.hypot(a, b)
+        return math.hypot(a, b)
     except:
-        print("Invalid")
+        raise
 
 def add(a, b):
     return a + b
@@ -35,5 +35,5 @@ def logarithm(a, b):
     else:
         return math.log(b, a)
 
-def exponent(a, b):
+def exp(a, b):
     return a**b
