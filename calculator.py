674b066b314aabe2839c42018fea570fa9b25bc7
zgunning-sudo <zgunning@ufl.edu>
2026-04-08 10:10:21 -0400

    Part 1 task

--- calculator.py
+++ calculator.py
@@ -4,8 +4,31 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
-    pass
 
+import math
 
+def add(a, b):
+    return a + b
+
+def subtact(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    else:
+        return b / a
+
+def logarithm(a, b):
+    if a < 0 or a == 1:
+        raise ValueError
+    elif b <= 0:
+        raise ValueError
+    else:
+        return math.log(b, a)
+
+def exponent(a, b):
+    return a**b
